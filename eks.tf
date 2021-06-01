239d9208ed016c9f7d335c37059e9e22f9b81e72
Greg Sharpe <git+me@gregsharpe.co.uk>
2021-06-01 16:35:57 +0100

    Lets try that

--- eks.tf
+++ eks.tf
@@ -19,8 +19,7 @@ provider kubernetes {
 }
 
 module cluster {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = ">= 1.13"
+  source          = "terraform-aws-modules/eks/aws?ref=13.0.0"
 
   cluster_name    = local.stack
   cluster_version = "1.16"
